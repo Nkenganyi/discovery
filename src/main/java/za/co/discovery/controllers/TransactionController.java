2908aaf74ac96a5f4ce33c1978d21f6d6fabb228
nkeng <nkeng@nkeng-PC>
2019-12-20 23:35:17 +0200

    transaction

---
+++ src/main/java/za/co/discovery/controllers/TransactionController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.co.discovery.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author nkeng
+ */
+
+@RestController
+@RequestMapping("/discovery")
+public class TransactionController {
+    
+    public void withdrawal(){
+    
+    }
+    
+}
